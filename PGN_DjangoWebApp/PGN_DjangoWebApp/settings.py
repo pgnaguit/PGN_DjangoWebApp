7d59a495b6663cf0e728af7652092881944e199a
pgnaguit <pgmnaguit.student@ua.edu.ph>
2026-09-15 21:00:14 +0800

    Add authenticated Student API

--- PGN_DjangoWebApp/PGN_DjangoWebApp/settings.py
+++ PGN_DjangoWebApp/PGN_DjangoWebApp/settings.py
@@ -127,3 +127,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+LOGIN_REDIRECT_URL = '/registration/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- PGN_DjangoWebApp/PGN_DjangoWebApp/urls.py
+++ PGN_DjangoWebApp/PGN_DjangoWebApp/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path(
         'registration/',
         include('registration.urls')
+    ),
+
+    path(
+        'api/',
+        include('registration.api_urls')
+    ),
+
+    path(
+        'accounts/',
+        include('django.contrib.auth.urls')
     ),
 ]
